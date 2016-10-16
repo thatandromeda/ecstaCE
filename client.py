cdc19b5002938c3372e0117ff744eb7cdc6cabb4
Andromeda Yelton <andromeda.yelton@gmail.com>
2016-10-16 17:18:08 -0400

    Add minimally working client
    This works, because it issues a request which returns a 200 response.
    Also it fails, because I haven't generated an auth token. Onward and
    upward!

--- client.py
+++ client.py
@@ -1,1 +1,14 @@
 # connect to Moodle instance
+import requests
+
+MOODLE_BASE_URL = 'http://localhost/~ayelton/moodle'
+MOODLE_API_ENDPOINT = '/webservice/rest/server.php'
+MOODLE_AUTH_TOKEN = ''
+MOODLE_COURSE_CREATE = 'core_course_create_courses'
+
+url = '{domain}{endpoint}'.format(
+    domain=MOODLE_BASE_URL, endpoint=MOODLE_API_ENDPOINT)
+payload = {'wstoken': MOODLE_AUTH_TOKEN, 'wsfunction': MOODLE_COURSE_CREATE}
+response = requests.get(url, params=payload)
+print response
+print response.content
