eecd6738a462e9e2b3cca97467ae8238f5e6e28b
Andromeda Yelton <andromeda.yelton@gmail.com>
2016-10-18 09:57:51 -0400

    Move course action into a function so we can call it from elsewhere

--- client.py
+++ client.py
@@ -7,21 +7,22 @@ MOODLE_BASE_URL = 'http://localhost/~ayelton/moodle'
 MOODLE_API_ENDPOINT = '/webservice/rest/server.php'
 MOODLE_COURSE_CREATE = 'core_course_create_courses'
 
+MOODLE_URL = '{domain}{endpoint}'.format(
+        domain=MOODLE_BASE_URL, endpoint=MOODLE_API_ENDPOINT)
+
+
 # See https://github.com/moodle/moodle/blob/f73f93846940168fc79ee83036e977a0b3cfcbef/course/externallib.php
 # for available course parameters
 # See also https://github.com/squidsoup/muddle.py/blob/master/muddle/api.py,
 # which sadly seems to be no longer pip installable.
 
-url = '{domain}{endpoint}'.format(
-    domain=MOODLE_BASE_URL, endpoint=MOODLE_API_ENDPOINT)
-payload = {'wstoken': MOODLE_AUTH_TOKEN,
-           'wsfunction': MOODLE_COURSE_CREATE,
-           'moodlewsrestformat': 'json',
-          'courses[0][fullname]': 'Test course 1',
-          'courses[0][shortname]': 'test',
-          'courses[0][categoryid]': 1}
-# Should return something like [{"id":2,"shortname":"test"}]
+def create_course(fullname, shortname, categoryid=1):
+    payload = {'wstoken': MOODLE_AUTH_TOKEN,
+               'wsfunction': MOODLE_COURSE_CREATE,
+               'moodlewsrestformat': 'json',
+              'courses[0][fullname]': fullname,
+              'courses[0][shortname]': shortname,
+              'courses[0][categoryid]': categoryid}
+    # Should return something like [{"id":2,"shortname":"test"}]
 
-response = requests.get(url, params=payload)
-print response
-print response.content
+    response = requests.get(MOODLE_URL, params=payload)
